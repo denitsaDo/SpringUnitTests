790e4e5c9d99de66e16528fa8100568c108510ff
Denitsa Dochkova <Denitsa.Dochkova@gmail.com>
2022-06-16 17:12:04 +0300

    getByIdShould_ReturnBeer_WhenBeerExists() method added

--- src/test/java/com/telerikacademy/springdemo/services/BeersServiceImplTests.java
+++ src/test/java/com/telerikacademy/springdemo/services/BeersServiceImplTests.java
@@ -1,4 +1,33 @@
 package com.telerikacademy.springdemo.services;
 
+import com.telerikacademy.springdemo.models.Beer;
+import com.telerikacademy.springdemo.repositories.BeersRepository;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class BeersServiceImplTests {
+
+    @Test
+    public void getByIdShould_ReturnBeer_WhenBeerExists() {
+        //Arrange
+        BeersRepository repository = Mockito.mock(BeersRepository.class);
+        BeersService mockService = new BeersServiceImpl(repository);
+        Mockito.when(repository.getById(1))
+                .thenReturn(new Beer(1, "testBeer", 1.1));
+        //Act
+        Beer returnedBeer = mockService.getById(1);
+
+        //Assert
+        Assert.assertEquals(returnedBeer.getId(), 1);
+    }
+
+//    @Test
+//    public void getByIdShould_ReturnBeer_WhenBeerExists() {
+//        //Arrange
+//
+//        //Act
+//
+//        //Assert
+//    }
 }
